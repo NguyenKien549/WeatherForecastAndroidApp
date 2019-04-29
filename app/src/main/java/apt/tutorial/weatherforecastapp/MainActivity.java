73704b71f11a35540a242545a1fe201002a8efaf
tai221 <tai2211998@gmail.com>
2019-04-29 18:51:29 +0700

    thêm giao diện 5 ngày

--- app/src/main/java/apt/tutorial/weatherforecastapp/MainActivity.java
+++ app/src/main/java/apt/tutorial/weatherforecastapp/MainActivity.java
@@ -4,13 +4,23 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
-
+import android.graphics.Color;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    LineChart mpLineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,67 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.custom_action_bar);
 
+        mpLineChart=(LineChart)findViewById(R.id.line_chart);
+        LineDataSet lineDataSet1=new LineDataSet(dataValues1(),"");
+        LineDataSet lineDataSet2=new LineDataSet(dataValues2(),"");
+        ArrayList<ILineDataSet> dataSets=new ArrayList<>();
+        dataSets.add(lineDataSet1);
+        dataSets.add(lineDataSet2);
+//        mpLineChart.setBackgroundColor(Color.BLUE);
+        mpLineChart.setDrawGridBackground(false);
+        mpLineChart.setDrawBorders(false);
+        mpLineChart.getAxisLeft().setDrawGridLines(false);
+        mpLineChart.getXAxis().setDrawGridLines(false);
+
+        mpLineChart.getAxisRight().setDrawGridLines(false);
+        mpLineChart.getXAxis().setDrawGridLines(false);
+        mpLineChart.getXAxis().setEnabled(false);
+        mpLineChart.getAxisRight().setEnabled(false);
+        mpLineChart.getAxisLeft().setEnabled(false);
+
+        mpLineChart.getAxisLeft().setDrawAxisLine(false);
+        mpLineChart.getAxisRight().setDrawAxisLine(false);
+
+        mpLineChart.getLegend().setEnabled(false);
+        Description des=new Description();
+        des.setText("");
+        mpLineChart.setDescription(des);
+
+        LineData data=new LineData(dataSets);
+        data.setValueFormatter(new MyValueFormat());
+        mpLineChart.setData(data);
+        mpLineChart.invalidate();
     }
+
+    private ArrayList<Entry> dataValues1(){
+        ArrayList<Entry> dataVals=new ArrayList<Entry>();
+        dataVals.add(new Entry(0,20));
+        dataVals.add(new Entry(1,23));
+        dataVals.add(new Entry(2,22));
+        dataVals.add(new Entry(3,25));
+        dataVals.add(new Entry(4,29));
+
+
+        return dataVals;
+    }
+
+    private ArrayList<Entry> dataValues2(){
+        ArrayList<Entry> dataVals=new ArrayList<Entry>();
+        dataVals.add(new Entry(0,30));
+        dataVals.add(new Entry(1,33));
+        dataVals.add(new Entry(2,32));
+        dataVals.add(new Entry(3,35));
+        dataVals.add(new Entry(4,39));
+
+
+        return dataVals;
+    }
+
+    private  class MyValueFormat implements IValueFormatter{
+        @Override
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            return value+" °C";
+        }
+    }
+
 }
