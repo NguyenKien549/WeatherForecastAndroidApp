94dc346f45f2dd32e3b03783872bca720a6ba6a9
= <=>
2019-05-09 01:12:48 +0700

    custom listview 24h next

---
+++ app/src/main/java/apt/tutorial/weatherforecastapp/OneHourInfo.java
@@ -0,0 +1,31 @@
+package apt.tutorial.weatherforecastapp;
+
+public class OneHourInfo {
+    private int temp;
+    private String stateIcon;
+    private int time;
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public String getStateIcon() {
+        return stateIcon;
+    }
+
+    public void setStateIcon(String stateIcon) {
+        this.stateIcon = stateIcon;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+}
