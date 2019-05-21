6f7ba8aca3b59b974ef86661196bc612b2d51cae
tai221 <tai2211998@gmail.com>
2019-05-21 09:49:30 +0700

    nothing change

--- app/src/main/java/apt/tutorial/weatherforecastapp/SearchActivity.java
+++ app/src/main/java/apt/tutorial/weatherforecastapp/SearchActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Place;
